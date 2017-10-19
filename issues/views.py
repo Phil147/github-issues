9080288846aaac89c92cd186cb0a47f58b1a8d79
Philipp Fahrenschon <ph.fahrenschon@gmail.com>
2017-10-19 21:27:27 +0200

    added logic into IssueCreateView to call the api and handle the response

--- issues/views.py
+++ issues/views.py
@@ -1,10 +1,26 @@
 from django.views.generic import TemplateView, FormView
+from django.contrib import messages
+from django.utils.safestring import mark_safe
+from django.urls import reverse_lazy
+
 from issues.forms import IssueForm
+from issues.utils.github import create_issue
 
 
 class IssueCreateView(TemplateView, FormView):
     template_name = 'issues/add_issue.html'
     form_class = IssueForm
+    success_url = reverse_lazy('add-issue')
 
     def form_valid(self, form):
-        pass
+        r = create_issue(form.cleaned_data['title'], form.cleaned_data['body'])
+        response = r.json()
+        if r.status_code != 201:
+            message = 'Oops something went wrong:<br>{}'.format(response['message'])
+            message_level = messages.ERROR
+        else:
+            message = mark_safe(
+                'Issue #{} created. Have a look <a href="{}">here</a>'.format(response['number'], response['html_url']))
+            message_level = messages.SUCCESS
+        messages.add_message(self.request, message_level, message)
+        return super().form_valid(form)
